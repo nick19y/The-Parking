f112aa84bad94fc965cb93b130606a6722fbefc7
nick19y <godoi.nic.nic@gmail.com>
2023-01-27 20:24:05 -0300

    início do desenvolvimento

---
+++ trabalho/registro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="cabecalho-principal">
+        <div class="imagem-usuario">
+            <img src="img/usuario.svg" alt="Imagem do usuário" >
+            <h1>teste</h1>
+        </div>
+        <div class="mensagem-data">
+            <h1 class="mensagem-usuario">Bem-vindo novamente, Enzo!</h1>
+            <div class="container-data">
+                <h3 class="dia-mes"></h3>
+                <div class="semana-horario">
+                    <div class="dia-semana"></div>
+                    <div class="horario"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
