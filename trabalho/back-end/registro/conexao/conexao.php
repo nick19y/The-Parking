6b65b178b5a233f95dd0796d050c37ffab5128ef
nick19y <godoi.nic.nic@gmail.com>
2023-03-13 18:12:35 -0300

    codeigniter

--- trabalho/back-end/registro/conexao/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-        $servidor = "localhost";
-        $bd = "estacionamento_mariah";
-        $usuario = "root";
-        $senha = "";
-    
-        $conexao = new PDO("mysql:host=$servidor; dbname=$bd", $usuario, $senha,
-        array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8"));
-?>
