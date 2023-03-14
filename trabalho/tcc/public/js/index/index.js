c37e035f21e9e4c98e35a41ab1d4d0fe30e7f6bd
nick19y <godoi.nic.nic@gmail.com>
2023-03-14 10:37:34 -0300

    organização do repositório

--- trabalho/tcc/public/js/index/index.js
+++
@@ -1,9 +0,0 @@
-const btnIniciarSessao = document.querySelector(".btn-iniciar");
-const btnCriarConta = document.querySelector(".btn-criar-conta");
-
-btnIniciarSessao.addEventListener("click", function(){
-    window.open("iniciarSessao.php", "_self");
-});
-btnCriarConta.addEventListener("click", function(){
-    window.open("criarConta.php", "_self");
-});
