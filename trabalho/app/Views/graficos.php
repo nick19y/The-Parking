e198d00f8c04dc5275abcf3ecf501a40ebb8fc11
nick19y <godoi.nic.nic@gmail.com>
2023-04-29 18:47:25 -0300

    novo-grafico

--- trabalho/app/Views/graficos.php
+++ trabalho/app/Views/graficos.php
@@ -1,31 +1,23 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
-    <link rel="stylesheet" href="/css/styles.css">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gráficos</title>
     <link rel="stylesheet" href="/css/graficos/graf.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.1.1/chart.umd.js"></script>
+    <link rel="stylesheet" href="/css/styles.css">
     <script src="/js/graficos/graficos.js" defer></script>
     <script src="/js/botao-voltar/botao-voltar.js" defer></script>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gráficos</title>
-</head>
-<body>
-    <main class="painel-inicial-grafico imagem-de-fundo">
-    <button class="btn-voltar-sessao"><img src="img/up.svg" alt=""></button>
-        <!-- <button class="botao-voltar">
-            <span class="span-voltar"><</span>
-        </button> -->
-    <div class="chartCard">
-        <div class="chartBox">
-            <canvas id="grafico" width="600" height="250"></canvas>
-        </div>
+  </head>
+  <body>
+    <div class="chartMenu">
+        <button class="btn-voltar-sessao"><img src="/img/up.svg" alt=""></button>
     </div>
-    </main>
-</body>
+    <div class="chartCard">
+      <div class="chartBox">
+        <canvas id="myChart"></canvas>
+      </div>
+    </div>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js"></script>
+  </body>
 </html>
--- trabalho/public/js/graficos/graficos.js
+++ trabalho/public/js/graficos/graficos.js
@@ -1,31 +1,50 @@
-const grafico = document.getElementById('grafico');
-console.log("Funciona");
-new Chart(grafico, {
+// https://www.youtube.com/watch?v=0VcybDX-kk0&ab_channel=ChartJS
+const data = {
+    labels: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    datasets: [{
+      label: 'Faturamento semanal',
+      data: [18, 12, 6, 9, 12, 3, 9],
+      backgroundColor: [
+        'rgba(255, 26, 104, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(0, 0, 0, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 26, 104, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(0, 0, 0, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  // config 
+  const config = {
     type: 'bar',
-    responsive: true,
-    data: {
-        labels: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
-        datasets: [{
-            label: 'Receita',
-            data: [500, 450, 300, 600, 200, 900, 500],
-            borderWidth: 5,
-            backgroundColor: [
-                '#F2C12E',
-                '#F2C12E',
-                '#F2C12E',
-            ]
-        }]
-    },
+    data,
     options: {
-        maintainAspectRatio: false,
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: { color: 'white', beginAtZero: true }
-            },
-            x: {
-                ticks: { color: 'white', beginAtZero: true }
-            }
+    maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
         }
+      }
     }
-});
+  };
+
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+
+  const chartVersion = document.getElementById('chartVersion');
+  chartVersion.innerText = Chart.version;
